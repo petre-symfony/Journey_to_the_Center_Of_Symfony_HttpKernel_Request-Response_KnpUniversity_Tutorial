d53cb3b35dfaa997054d769ed815e15ce9b4e47f
petrero <petrero@gmail.com>
2017-05-29 21:10:45 +0300

    Copy code from branch 1

---
+++ src/AppBundle/EventListener/UserAgentSubscriber.php
@@ -0,0 +1,30 @@
+<?php
+namespace AppBundle\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+
+class UserAgentSubscriber implements EventSubscriberInterface{
+  /**
+   * @var LoggerInterface
+   */
+  private $logger;
+  
+  public function __construct(LoggerInterface $logger) {
+    $this->logger = $logger;
+  }
+  
+  public function onKernelRequest(GetResponseEvent $event) {
+    $this->logger->info('RRRRAAAAAWWWWR');
+    $request = $event->getRequest();
+    $userAgent = $request->headers->get('User-Agent');
+    $this->logger->info('The user agent is: '.$userAgent);
+  }
+  
+  public static function getSubscribedEvents() {
+    return array(
+      'kernel.request' => 'onKernelRequest'
+    );
+  }
+}
