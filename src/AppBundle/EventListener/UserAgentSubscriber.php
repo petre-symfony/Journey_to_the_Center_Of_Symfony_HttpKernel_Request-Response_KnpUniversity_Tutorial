0564bd94eb8505e5cda0d2cffc03ea128feb70d1
petrero <petrero@gmail.com>
2017-05-30 18:38:51 +0300

    Copy code from branch 4

---
+++ src/AppBundle/EventListener/UserAgentSubscriber.php
@@ -0,0 +1,38 @@
+<?php
+namespace AppBundle\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserAgentSubscriber implements EventSubscriberInterface{
+  /**
+   * @var LoggerInterface
+   */
+  private $logger;
+  
+  public function __construct(LoggerInterface $logger) {
+    $this->logger = $logger;
+  }
+  
+  public function onKernelRequest(GetResponseEvent $event) {
+    $this->logger->info('RRRRAAAAAWWWWR');
+    $request = $event->getRequest();
+    $userAgent = $request->headers->get('User-Agent');
+    $this->logger->info('The user agent is: '.$userAgent);
+    
+    if(rand(0, 100) > 50){
+      $response = new Response('Come back later!');
+      //$event->setResponse($response);
+    }
+  }
+  
+  public static function getSubscribedEvents() {
+    return array(
+      //constant that means kernel.request  
+      KernelEvents::REQUEST => 'onKernelRequest'
+    );
+  }
+}
