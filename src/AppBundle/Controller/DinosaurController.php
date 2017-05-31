49a13924bde292d6e2cd079252bb02f894b54c4d
petrero <petrero@gmail.com>
2017-05-31 20:00:39 +0300

    Copy code from branch 8

--- src/AppBundle/Controller/DinosaurController.php
+++ src/AppBundle/Controller/DinosaurController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\Dinosaur;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class DinosaurController extends Controller {
   /**
---
+++ src/AppBundle/EventListener/UserAgentSubscriber.php
@@ -0,0 +1,45 @@
+<?php
+namespace AppBundle\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserAgentSubscriber implements EventSubscriberInterface{
+  /**
+   * @var LoggerInterface
+   */
+  private $logger;
+  
+  public function __construct(LoggerInterface $logger) {
+    $this->logger = $logger;
+  }
+  
+  public function onKernelRequest(GetResponseEvent $event) {
+    $this->logger->info('RRRRAAAAAWWWWR');
+    $request = $event->getRequest();
+    $userAgent = $request->headers->get('User-Agent');
+    $this->logger->info('The user agent is: '.$userAgent);
+    
+    if(rand(0, 100) > 50){
+      $response = new Response('Come back later!');
+      //$event->setResponse($response);
+    }
+    
+    /*
+    $request->attributes->set('_controller', function($id){
+      return new Response('Hello ' . $id);
+    });
+    */
+    
+  }
+  
+  public static function getSubscribedEvents() {
+    return array(
+      //constant that means kernel.request  
+      KernelEvents::REQUEST => 'onKernelRequest'
+    );
+  }
+}
