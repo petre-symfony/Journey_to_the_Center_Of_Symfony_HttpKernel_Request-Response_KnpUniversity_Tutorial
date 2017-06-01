3fd609d3133e051733ceedf722a63cad51af2d42
petrero <petrero@gmail.com>
2017-06-01 09:04:47 +0300

    Copy code from branch 9

--- src/AppBundle/Controller/DinosaurController.php
+++ src/AppBundle/Controller/DinosaurController.php
@@ -5,18 +5,20 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\Dinosaur;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class DinosaurController extends Controller {
   /**
    * @Route("/", name="dinosaur_list")
    */
-  public function indexAction(){
+  public function indexAction($isLinux){
     $dinos = $this->getDoctrine()
       ->getRepository('AppBundle:Dinosaur')
       ->findAll();
 
     return $this->render('dinosaurs/index.html.twig', [
       'dinos' => $dinos,
+      'isLinux' => $isLinux  
     ]);
   }
 
